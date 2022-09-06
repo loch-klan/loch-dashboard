1dbfd0cf30b61ccc5e0d5f963722c4de44fd73f2
DhwaniHingu <darshakg81@gmail.com>
2022-09-06 17:32:20 +0100

    Update

--- src/app/Portfolio/Portfolio.js
+++ src/app/Portfolio/Portfolio.js
@@ -2,17 +2,53 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import Sidebar from '../common/Sidebar';
-
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
 import WelcomeCard from './WelcomeCard';
+
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 class Portfolio extends Component {
     constructor(props) {
         super(props);
         this.state = {}
+        
     }
+    
 
     componentDidMount() { }
+    
 
     render() {
+
+        const data = {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [
+              {
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+              },
+            ],
+          };
+          
         return (
             <div className="portfolio-page-section">
                 <Sidebar ownerName="" />
@@ -23,6 +59,7 @@ class Portfolio extends Component {
                     <div className='portfolio-section page'>
                         <div className=''>
                             <h1 className='Inter-Medium'>Overview</h1>
+                            <Doughnut data={data} />
                         </div>
                     </div>
                 </div>
@@ -30,6 +67,7 @@ class Portfolio extends Component {
         )
     }
 }
+
 
 const mapStateToProps = state => ({
     portfolioState: state.PortfolioState
