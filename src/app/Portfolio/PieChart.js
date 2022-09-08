a41f253ab6106129956682673d0b6cd3209cbf60
DhwaniHingu <darshakg81@gmail.com>
2022-09-09 00:32:15 +0100

    Chart Bug Fix and update

---
+++ src/app/Portfolio/PieChart.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from "react-redux";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+class PieChart extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PieChart;
--- src/app/Portfolio/Portfolio.js
+++ src/app/Portfolio/Portfolio.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import Sidebar from '../common/Sidebar';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
 import WelcomeCard from './WelcomeCard';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { ReactReduxContext } from 'react-redux';
 
 
-ChartJS.register(ArcElement, Tooltip, Legend);
 
 class Portfolio extends Component {
     constructor(props) {
@@ -22,72 +19,15 @@ class Portfolio extends Component {
 
     componentDidMount() { }
 
-
     render() {
-
-        // const data = {
-        //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        //     datasets: [{
-        //         label: '# of Votes',
-        //         data: [12, 19, 3, 5, 2, 3],
-        //         backgroundColor: [
-        //             'rgba(255, 99, 132, 0.2)',
-        //             'rgba(54, 162, 235, 0.2)',
-        //             'rgba(255, 206, 86, 0.2)',
-        //             'rgba(75, 192, 192, 0.2)',
-        //             'rgba(153, 102, 255, 0.2)',
-        //             'rgba(255, 159, 64, 0.2)',
-        //         ],
-        //         borderColor: [
-        //             'rgba(255, 99, 132, 1)',
-        //             'rgba(54, 162, 235, 1)',
-        //             'rgba(255, 206, 86, 1)',
-        //             'rgba(75, 192, 192, 1)',
-        //             'rgba(153, 102, 255, 1)',
-        //             'rgba(255, 159, 64, 1)',
-        //         ],
-        //         borderWidth: 1,
-        //     }],
-        // };
-
-        // const options = {
-        //     cutout: "75%",
-        //     plugins: {
-        //         legend: {
-        //             display: false
-        //         },
-        //         tooltip: {
-        //             enabled: false
-        //         },
-        //         events: false,
-        //         animation: {
-        //             duration: 0
-        //         },
-        //     },
-        //     responsive: true,
-        // }
-
         const options = {
-            // colors: ['rgba(255, 99, 132, 0.2)',
-            //     'rgba(54, 162, 235, 0.2)',
-            //     'rgba(255, 206, 86, 0.2)',
-            //     'rgba(75, 192, 192, 0.2)'],
             chart: {
                 styledMode: false,
                 type: 'pie',
                 backgroundColor: null,
                 //height: (9 / 16 * 100) + '%',
                 width: 1000,
-
                 events: {
-                    mouseOver: (event) => console.log('hhiii'),
-                    // mouseOver: function () {
-                    //     console.log('hi');
-
-                    // },
-                    mouseOut: function () {
-                        console.log("xD");
-                    },
                     render: function () {
                         var series = this.series[0],
                             seriesCenter = series.center,
@@ -114,9 +54,7 @@ class Portfolio extends Component {
                         });
                     }
                 }
-
             },
-
             accessibility: {
 
                 point: {
@@ -126,6 +64,9 @@ class Portfolio extends Component {
             title: {
                 text: null
             },
+            credits: {
+                enabled: false
+            },
             tooltip: {
                 enabled: false,
                 pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
@@ -133,74 +74,75 @@ class Portfolio extends Component {
             plotOptions: {
                 pie: {
                     size: 600,
-                    // /allowPointSelect: true,
                     cursor: 'pointer',
-                    // showInLegend: true,
-
                     connectorPadding: 2,
+                    states: {
+                        inactive: {
+                            opacity: 0,
+                            enabled: true,
+                        }
+                    },
+                    point: {
+                        events: {
+                            mouseOver: function () {
+                                let color = this.options.borderColor;
+                                this.update({ color: color });
+                                var currentData = this;
+                                this.series.data.forEach(function (data) {
+                                    if (currentData.id !== data.id) {
+                                        data.update({ opacity: 0.2 }, true);
+                                    } else {
+                                        data.update({ opacity: 1 }, false);
+                                    }
+                                })
+                            },
+                            mouseOut: function () {
+                                let color = this.options.originalColor;
+                                this.update({ color: color });
+                                this.series.data.forEach(function (data) {
+                                    data.update({ opacity: 1 }, false);
+                                });
+                            }
+                        },
+                    },
                     dataLabels: {
                         distance: 0,
                         tickWidth: 0,
-                        padding: 6,
-                        format: '<span style="color:{point.borderColor}">\u25CF &nbsp;&nbsp;</span><p class="inter-display-regular f-s-16" style="fill:#5B5B5B">{point.name} &nbsp;</p> <p class="inter-display-regular f-s-16 " style="fill:#B0B1B3">${point.usd} USD&nbsp;</p> <p class="inter-display-medium f-s-16" style="fill:#B0B1B3"> {point.y}%</p>',
-
-                        // formatter: function () {
-                        //     return '<span style="color:'+this.point.borderColor+'>Yash &nbsp;&nbsp;</span>'
-                        //         + '<p class="inter-display-regular f-s-16" style="fill:#5B5B5B">'+this.point.name +'&nbsp;&nbsp;</p>'
-                        //         + '<p class="inter-display-regular f-s-16 " style="fill:#B0B1B3">$'+this.point.usd +'USD&nbsp;&nbsp;</p>'
-                        //         + '<p class="inter-display-medium f-s-16" style="fill:#B0B1B3">'+this.point.y+'%</p>'
-
-                        // },
+                        padding: 12,
+                        format: '<span class="f-s-16" style="color:{point.borderColor};">\u25CF &nbsp;</span><p class="inter-display-regular f-s-16 test1" style="fill:#5B5B5B">{point.name}&nbsp;</p> <p class="inter-display-regular f-s-16 test1" style="fill:#B0B1B3">${point.usd} USD&nbsp;</p><p class="inter-display-medium f-s-16 test1" style="fill:#B0B1B3"> {point.y}% &nbsp;&nbsp;</p>',
                         backgroundColor: '#FFFFFF',
                         enabled: true,
                         crop: false,
                         color: '#636467',
-                        // borderWidth: .5,
                         borderRadius: 8,
-
                         verticalAlign: 'top',
                         style: {
-
-                            //textOverflow: 'none'
-                            // fontFamily: 'Inter-Regular',
-                            // fontSize: '16px',
-                            // fontWeight: 400,
-                            // textShadow: 'none',
                             textOverflow: 'clip',
-                            // width: 300,
-                            // height: 20,
                             whiteSpace: 'nowrap'
                         }
                     },
-                    states: {
-                        hover: {
-                            enabled: true,
-                            color: 'red'
-                        }
-                    }
                 }
             },
             series: [{
                 name: 'Registrations',
-                // colorByPoint: true,
                 innerSize: '75%',
                 size: "100%",
                 data: [{
                     name: 'Bitcoin',
                     y: 68.1,
-                    usd: "222,798.00",
+                    usd: "222,798",
                     borderColor: 'rgba(255, 99, 132, 1)',
                     borderWidth: 2,
-                    color: 'rgba(255, 99, 132, 0.2)'
+                    color: 'rgba(255, 99, 132, 0.2)',
+                    originalColor: 'rgba(255, 99, 132, 0.2)'
                 }, {
                     name: 'Ethereum',
                     y: 11.0,
                     usd: "55,143",
                     borderColor: 'rgba(54, 162, 235, 1)',
                     borderWidth: 2,
-                    color: 'rgba(54, 162, 235, 0.2)'
-                    //     'rgba(255, 206, 86, 0.2)',
-                    //     'rgba(75, 192, 192, 0.2)'],
+                    color: 'rgba(54, 162, 235, 0.2)',
+                    originalColor: 'rgba(54, 162, 235, 0.2)'
 
                 }, {
                     name: 'Solana',
@@ -208,33 +150,37 @@ class Portfolio extends Component {
                     usd: "19,925",
                     borderColor: 'rgba(255, 206, 86, 1)',
                     borderWidth: 2,
-                    color: 'rgba(255, 206, 86, 0.2)'
+                    color: 'rgba(255, 206, 86, 0.2)',
+                    originalColor: 'rgba(255, 206, 86, 0.2)'
                 },
-                 {
+                {
                     name: 'Avalanche',
                     y: 2.5,
                     usd: "6,303",
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 2,
-                    color: 'rgba(75, 192, 192, 0.2)'
+                    color: 'rgba(75, 192, 192, 0.2)',
+                    originalColor: 'rgba(75, 192, 192, 0.2)'
                 },
                 {
-                  name: 'Solana',
-                  y: 2.5,
-                  usd: "19,925",
-                  borderColor: 'rgba(255, 206, 86, 1)',
-                  borderWidth: 2,
-                  color: 'rgba(255, 206, 86, 0.2)'
-              },
+                    name: 'Solana',
+                    y: 2.5,
+                    usd: "19,925",
+                    borderColor: 'rgba(255, 206, 86, 1)',
+                    borderWidth: 2,
+                    color: 'rgba(255, 206, 86, 0.2)',
+                    originalColor: 'rgba(255, 206, 86, 0.2)'
+                },
                 {
-                  name: 'Avalanche',
-                  y: 4.7,
-                  usd: "6,303",
-                  borderColor: 'rgba(75, 192, 192, 1)',
-                  borderWidth: 2,
-                  color: 'rgba(75, 192, 192, 0.2)'
-              }
-              ]
+                    name: 'Avalanche',
+                    y: 4.7,
+                    usd: "6,303",
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 2,
+                    color: 'rgba(75, 192, 192, 0.2)',
+                    originalColor: 'rgba(75, 192, 192, 0.2)'
+                }
+                ]
             }]
         };
 
