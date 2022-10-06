aeafe38156fe8c4270856a8cabc3fd8c4b55a139
Harshang <harshangmak01@gmail.com>
2022-10-06 19:00:03 +0530

    Subject : Wallet chain api integration and minor changes in css

--- src/app/common/PageHeader.js
+++ src/app/common/PageHeader.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 export default function PageHeader(props) {
   return (
-    <div className='m-b-36 page-header'>
+    <div className='m-b-45 page-header'>
     <div className='header'>
-        <h4 className='inter-display-medium f-s-31 lh-37 m-b-12'>{props.title}</h4>
+        <h4 className='inter-display-medium f-s-25 lh-30 m-b-8'>{props.title}</h4>
         <p className="inter-display-medium f-s-16 lh-19">{props.subTitle}</p>
     </div>
     {props.btnText && <Button className='primary-btn'>{props.btnText}</Button>}
