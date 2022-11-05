e9ba613d2524727b3016e1fef85bfff08ab3896c
teamzero-aaq <abdulquadir828@hmail.com>
2022-11-05 16:15:08 +0530

    Layout Fixed

--- src/app/common/BarGraphFooter.js
+++ src/app/common/BarGraphFooter.js
@@ -6,18 +6,28 @@ export const BarGraphFooter = (props) => {
   const timeSeries = props.footerLabels ? props.footerLabels : ["All time","5 Years","1 Year","6 Months","1 Month","1 Week"]
 
   const timeBadge = timeSeries.map((badge,index)=>{
-      const className = index==props.active ? "inter-display-medium f-s-13 lh-16 m-r-20 timeBadge  active" : "inter-display-medium f-s-13 lh-16 m-r-20 timeBadge"
-    return(
-      <Col md={2} key={index}>
-        <div  id={index} className={className} onClick={props.handleFooterClick}>{badge}</div>
-      </Col>
-    )
+      const className = index==props.active ? "inter-display-medium f-s-13 lh-16 timeBadge  active" : "inter-display-medium f-s-13 lh-16 timeBadge"
+    return (
+      // <Col md={4} sm={6} lg={2} key={index}>
+      //   <div  id={index} className={className} onClick={props.handleFooterClick}>{badge}</div>
+      // </Col>
+
+      <div
+        key={index}
+        id={index}
+        className={className}
+        onClick={props.handleFooterClick}
+      >
+        {badge}
+      </div>
+    );
   })
   return (
-    <div className='bar-graph-footer'>
-      <Row>
+    <div className="bar-graph-footer">
+      {/* <Row>
         {timeBadge}
-      </Row>
+      </Row> */}
+      <div className='timeBadgeWrapper'>{timeBadge}</div>
     </div>
-  )
+  );
 }
