f479091a713928923f3ce1337379f815f27047a3
Nimesh <nimesh.sakhiya2003@gmail.com>
2022-11-07 23:52:15 +0530

    subject - pagination from user input

--- src/utils/commonComponent/Pagination.js
+++ src/utils/commonComponent/Pagination.js
@@ -4,38 +4,71 @@ import { Image } from "react-bootstrap";
 import pagePrev from '../../assets/images/page-prev.svg';
 import pageNext from '../../assets/images/page-next.svg';
 
-const onLeftClick = (props) => {
-  if (props.noUrl) {
-    props.loadData(props.page - 1);
-  } else {
-    if (props.page > 1) {
-      const params = new URLSearchParams(props.location.search);
-      params.set("p", props.page - 2);
-      props.history.push(`${props.history.location.pathname}?${params}`);
-      // props.loadData(props.page - 1);
-    }
-  }
-
-}
-
-const onNextClick = (props) => {
-  if (props.noUrl) {
-    props.loadData(props.page + 1);
-  } else {
-    if (props.page < props.pageCount) {
-      const params = new URLSearchParams(props.location.search);
-      params.set("p", props.page);
-      props.history.push(`${props.history.location.pathname}?${params}`)
-      // props.loadData(props.page + 1);
-    }
-  }
-}
-
 const Pagination = props => {
+
+  const [message, setMessage] = React.useState(props.page);
+  
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' && message!==0) {
+      onSubumit(props)
+    }
+    
+  };
+  const onSubumit = (props) => {
+    if (message < props.pageCount) {
+        const params = new URLSearchParams(props.location.search);
+        params.set("p", message-1);
+        props.history.push(`${props.history.location.pathname}?${params}`)
+      }
+  }
+  
+  const onLeftClick = (props) => {
+    var pageNo = document.getElementById("pageNo")
+
+    if (props.noUrl) {
+      props.loadData(props.page - 1);
+    } else {
+      if (props.page > 1) {
+        const params = new URLSearchParams(props.location.search);
+        params.set("p", props.page - 2);
+        props.history.push(`${props.history.location.pathname}?${params}`);
+        // props.loadData(props.page - 1);
+        pageNo.value = (props.page-1)
+      }
+    }
+  
+  }
+  
+  const onNextClick = (props) => {
+    var pageNo = document.getElementById("pageNo")
+  
+    if (props.noUrl) {
+      props.loadData(props.page + 1);
+    } else {
+      if (props.page < props.pageCount) {
+        const params = new URLSearchParams(props.location.search);
+        params.set("p", props.page);
+        props.history.push(`${props.history.location.pathname}?${params}`);
+        // props.loadData(props.page + 1);
+        pageNo.value=(props.page+1);
+      }
+    }
+  
+  }
+  
   return (
     <div className="pagination-wrapper">
       <Image src={props.pagePrev ? props.pagePrev : pagePrev} onClick={() => onLeftClick(props)} className="left-arrow" />
-      <h5 className='inter-display-medium f-s-14'>{props.page} of {props.pageCount}</h5>
+      <h5 className='inter-display-medium f-s-14'>
+        <input type="number" 
+        name="pagenumber" 
+        id="pageNo" 
+        defaultValue={props.page}
+        onChange={(event)=>{setMessage(event.target.value)}}
+        onKeyDown={handleKeyDown}
+        />
+        of {props.pageCount}
+      </h5>
       <Image src={props.pageNext ? props.pageNext : pageNext} onClick={() => onNextClick(props)} className="right-arrow" />
     </div >
   );
