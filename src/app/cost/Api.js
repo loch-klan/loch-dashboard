a04197ac928f9a9f11c479e223ee5c175a7c6998
teamzero-aaq <abdulquadir828@hmail.com>
2023-01-10 21:07:26 +0530

    Counter part chart fixed

--- src/app/cost/Api.js
+++ src/app/cost/Api.js
@@ -30,11 +30,23 @@ export const getAllCounterFeeApi = (ctx, startDate, endDate) => {
     }
     postLoginInstance.post("wallet/transaction/get-counter-party-volume-traded", data)
     .then((res) => {
-      if(!res.data.error){
+      if (!res.data.error) {
+        
+        let g_data = res.data.data.counter_party_volume_traded.sort((a, b) => {
+          return b.total_volume - a.total_volume;
+        });
+        g_data = g_data.slice(0,3);
+        // console.log("data", g_data)
         ctx.setState({
           counterGraphLoading: false,
           counterPartyData: res.data.data.counter_party_volume_traded,
-          counterPartyValue: getCounterGraphData(res.data.data.counter_party_volume_traded, ctx)
+          counterPartyValue:
+            ctx.state.currentPage === "Home"
+              ? getCounterGraphData(g_data,ctx)
+              : getCounterGraphData(
+                  res.data.data.counter_party_volume_traded,
+                  ctx
+                ),
         });
       } else{
         toast.error(res.data.message || "Something Went Wrong")
