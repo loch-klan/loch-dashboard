a6b58d30fc626382c7f86444a2c8fa3a29c8bae9
teamzero-aaq <abdulquadir828@hmail.com>
2023-02-03 11:31:46 +0530

    tooltip bg added

--- src/app/intelligence/stackGrapgh.js
+++ src/app/intelligence/stackGrapgh.js
@@ -19,72 +19,12 @@ const options = {
       display: false,
     },
     tooltip: {
-      displayColors: false,
-      backgroundColor: "#ffffff",
-      // fontColor: '#000000',
-      intersect: false,
-      color: "#000000",
-      padding: 12,
-      borderWidth: 1,
-      borderColor: "#E5E7EB",
-      // boxPadding: 5,
-      bodyFont: {
-        family: "Inter-Medium",
-        size: 13,
-      },
-      bodySpacing: 8,
-        callbacks: {
-            title: function (ctx) {
-                //   console.log("title", ctx);
-            }, //REMOVE TITLE
-            label: (ctx) => {
-                let label = ctx.label;
-                console.log("ctx", ctx);
-                let tooltip_title = "title";
-                let tooltipData = [];
-            
-                [...Array(4)].map(e => {
-                    tooltipData.push({
-                        name: "Name",
-                        x: 12,
-                        y: 123,
-                        color: "#2967E3",
-                    });
-                })
-               return `<div class="inter-display-semi-bold f-s-10 w-100 text-center"  style="color:#96979A; background-color:#ffffff; border: 1px solid #E5E5E6; border-radius:8px; margin-bottom:4px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04), 0px 1px 1px rgba(0, 0, 0, 0.04);
-backdrop-filter: blur(15px); padding:1rem 2rem;">Asset Breakdown</div>
-                  <div class="top-section py-4" style="background-color:#ffffff; border: 1px solid #E5E5E6; border-radius:10px;box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04), 0px 1px 1px rgba(0, 0, 0, 0.04);
-backdrop-filter: blur(15px);">
-                                <div class="tooltip-section-blue w-100" style="background-color:#ffffff; display:flex;
-                    flex-direction: column;
-                    justify-content: left;
-                    padding: 0.4rem 0.8rem;
-                    border-radius: 0.4rem;">
-                                <div class="inter-display-medium f-s-12 w-100 text-center px-4" style="color:#96979A;><b>${tooltip_title}</b></div><div class="w-100 mt-3" style="height: 1px; background-color: #E5E5E680;"></div>
-                                ${tooltipData
-                                  ?.map((item) => {
-                                    return `<div class="inter-display-medium f-s-13 w-100 pt-3 px-4">
-                                    <span style='width:10px; height: 10px; border-radius: 50%; background-color:${
-                                      item.color == "#ffffff"
-                                        ? "#16182B"
-                                        : item.color
-                                    }; display: inline-block; margin-right: 0.6rem'> </span>
-                                       ${item.name} <span style="color:${
-                                      item.color == "#ffffff"
-                                        ? "#16182B"
-                                        : item.color
-                                    }"> ${CurrencyType(false)}${numToCurrency(
-                                      item.y
-                                    )}</span>
-                                    </div>`;
-                                  })
-                                  .join(" ")}
-                            </div>
-                        </div>`;
-         
-                               
-        }
-        
+      intersect: true,
+      enabled: false,
+      external: (ctx) => {
+        console.log(ctx)
+
+        return `<div style="color:red;">title</div>`
       },
     },
   },
@@ -94,7 +34,7 @@ backdrop-filter: blur(15px);">
       //   min: 0,
       //   max: 500,
       stacked: false,
-      //   stacked: true,
+      // stacked: true,
       ticks: {
         display: true,
         // stepSize: 125,
@@ -114,6 +54,7 @@ backdrop-filter: blur(15px);">
     },
     x: {
       stacked: true,
+      offset: "none",
       ticks: {
         font: "Inter-SemiBold",
         size: 10,
