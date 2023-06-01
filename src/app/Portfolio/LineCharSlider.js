1fd913c4622f80b1adc865d021d8228c7766dacc
teamzero-aaq <abdulquadir828@hmail.com>
2023-06-01 20:23:13 +0530

    email hard code remove

--- src/app/Portfolio/LineCharSlider.js
+++ src/app/Portfolio/LineCharSlider.js
@@ -1259,7 +1259,7 @@ backdrop-filter: blur(15px);">
                   <span className="inter-display-semi-bold f-s-10 lh-12 grey-7C7 line-chart-dropdown-y-axis">
                     {CurrencyType()}
                   </span>
-                  {(this.props.dataLoaded || true) && (
+                  {this.props.dataLoaded && (
                       <div
                         style={{
                           position: "absolute",
