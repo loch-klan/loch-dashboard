01bee6ef24898b698b1aa6d77209eaa445c77eda
adityakumarloch <aditya@loch.one>
2023-07-25 13:34:32 +0530

    Net flow explainers removed

--- src/app/intelligence/Intelligence.js
+++ src/app/intelligence/Intelligence.js
@@ -138,6 +138,19 @@ class Intelligence extends Component {
   };
 
   componentDidMount() {
+    const tempLeftExplainerClosed = window.sessionStorage.getItem(
+      "netFlowLeftExplainerClosed"
+    );
+    if (tempLeftExplainerClosed) {
+      this.setState({ LeftShow: false });
+    }
+    const tempRightExplainerClosed = window.sessionStorage.getItem(
+      "netFlowRightExplainerClosed"
+    );
+    if (tempRightExplainerClosed) {
+      this.setState({ RightShow: false });
+    }
+
     if (this.props.location.hash !== "") {
       setTimeout(() => {
         const id = this.props.location.hash.replace("#", "");
@@ -601,6 +614,7 @@ class Intelligence extends Component {
   };
 
   RightClose = () => {
+    window.sessionStorage.setItem("netFlowRightExplainerClosed", true);
     this.setState({
       RightShow: false,
     });
@@ -612,6 +626,7 @@ class Intelligence extends Component {
   };
 
   LeftClose = () => {
+    window.sessionStorage.setItem("netFlowLeftExplainerClosed", true);
     this.setState({
       LeftShow: false,
     });
