02f986c021ae8fcab51a6ee8370f1c10784b5828
adityakumarloch <aditya@loch.one>
2023-08-04 17:44:32 +0530

    Text fixed

--- src/app/topAccount/top-transaction-history.js
+++ src/app/topAccount/top-transaction-history.js
@@ -1486,7 +1486,7 @@ class TopTransactionHistoryPage extends BaseReactComponent {
                   </Col>
                   <Col md={3}>
                     <CustomDropdown
-                      filtername="All Networks"
+                      filtername="All networks"
                       options={this.props.OnboardingState.coinsList}
                       action={SEARCH_BY_CHAIN_IN}
                       handleClick={this.handleFunction}
