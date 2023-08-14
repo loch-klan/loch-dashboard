4300b06f40bca81959261b1cfa227d29d1ea0242
adityakumarloch <aditya@loch.one>
2023-08-14 19:11:32 +0530

    New login log out in sidebar

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -8,5 +8,7 @@ export { default as BinVectorIcon } from "./binVector.svg";
 export { default as EyeIcon } from "./eyeIcon.svg";
 export { default as CloseIcon } from "./dummyX.svg";
 export { default as DeleteIcon } from "./delete-icon.png";
+export { default as GreyManIcon } from "./greyMan.svg";
+export { default as BlackManIcon } from "./blackMan.svg";
 export { default as CheckIcon } from "./check.svg";
 export { default as CompassWhiteIcon } from "./compassWhite.svg";
