772b1fe9861fc3e20f51ba8b99ceccdc426937d0
adityakumarloch <aditya@loch.one>
2023-08-17 13:17:43 +0530

    Netflows mixpannel events added

--- src/app/intelligence/Intelligence.js
+++ src/app/intelligence/Intelligence.js
@@ -96,8 +96,19 @@ class Intelligence extends Component {
       isGraphLoading: true,
       isChainSearchUsed: false,
       isAssetSearchUsed: false,
+      waitForMixpannelCall: false,
     };
   }
+  waitForMixpannelCallOn = () => {
+    this.setState({
+      waitForMixpannelCall: true,
+    });
+  };
+  waitForMixpannelCallOff = () => {
+    this.setState({
+      waitForMixpannelCall: false,
+    });
+  };
   chainSearchIsUsed = () => {
     this.setState({ isChainSearchUsed: true });
   };
