82f647d40bd1e8985c08c9d96b510b18a1fcba22
adityakumarloch <aditya@loch.one>
2023-12-08 17:29:25 +0530

    mobile crash fix

--- src/app/onboarding/addWallet.js
+++ src/app/onboarding/addWallet.js
@@ -339,9 +339,6 @@ class AddWallet extends BaseReactComponent {
   // };
 
   componentDidMount() {
-    if (mobileCheck()) {
-      this.props.history.push("/home");
-    }
     this.setState({
       addButtonVisible: this.state.walletInput.some((wallet) =>
         wallet.address ? true : false
