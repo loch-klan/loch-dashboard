c8693f8d04a2a78cde66e8896a5095e497a47a1a
adityakumarloch <aditya@loch.one>
2023-12-21 20:46:24 +0530

    Ui fix

--- src/app/smartMoney/homeSmartMoneyHeader.js
+++ src/app/smartMoney/homeSmartMoneyHeader.js
@@ -1,25 +1,13 @@
 import React, { useEffect } from "react";
-import { Dropdown, DropdownButton, Image } from "react-bootstrap";
-import logo from "../../image/Loch.svg";
+import { Image } from "react-bootstrap";
 import { getAllCurrencyApi, getAllCurrencyRatesApi } from "../common/Api";
 import {
-  BlackManIcon,
-  GreyManIcon,
   InfoCircleSmartMoneyIcon,
   PlusCircleSmartMoneyIcon,
   QuestionmarkCircleSmartMoneyIcon,
 } from "../../assets/images/icons";
-import CustomOverlay from "../../utils/commonComponent/CustomOverlay";
-
-import { toast } from "react-toastify";
-import { getCurrentUser } from "../../utils/ManageToken";
-import { BASE_URL_S3 } from "../../utils/Constant";
 
 export default function HomeSmartMoneyHeader(props) {
-  const [selectedCurrency, setCurrency] = React.useState(
-    JSON.parse(window.sessionStorage.getItem("currency"))
-  );
-  const [currencyList, setAllCurrencyList] = React.useState([]);
   const [localLochUser, setLocalLochUser] = React.useState(
     JSON.parse(window.sessionStorage.getItem("lochUser"))
   );
@@ -47,45 +35,15 @@ export default function HomeSmartMoneyHeader(props) {
           rate: 1,
         })
       );
-
-      setCurrency(JSON.parse(window.sessionStorage.getItem("currency")));
     }
 
     setTimeout(() => {
-      //  console.log("curr", currency);
       let currencyRates = JSON.parse(
         window.sessionStorage.getItem("currencyRates")
       );
-      // console.log("currency", currencyRates);
-      getAllCurrencyApi(setAllCurrencyList);
       !currencyRates && getAllCurrencyRatesApi();
     }, 1000);
   }, []); // <-- Have to pass in [] here!
-  const handleFunction = (currency) => {
-    let currencyRates = JSON.parse(
-      window.sessionStorage.getItem("currencyRates")
-    );
-    for (const [key, value] of Object.entries(currencyRates.rates)) {
-      // console.log(`${key}: ${value}`);
-      if (key === currency.code) {
-        currency = {
-          ...currency,
-          rate: value,
-        };
-      }
-    }
-    // MenuCurrencyDropdownSelected({
-    //   session_id: getCurrentUser().id,
-    //   email_address: getCurrentUser().email,
-    //   prev_currency: selectedCurrency.symbol + " " + selectedCurrency.code,
-    //   currency: currency.symbol + " " + currency.code,
-    // });
-    setTimeout(() => {
-      setCurrency(currency);
-      window.sessionStorage.setItem("currency", JSON.stringify(currency));
-      window.location.reload();
-    }, 200);
-  };
 
   return (
     <div className="homeSmartMoneyHeaderContainer">
