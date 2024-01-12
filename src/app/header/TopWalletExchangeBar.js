ac6ddc3cdd7f28a7dcea3eef9cc4f6484e8053d5
adityakumarloch <aditya@loch.one>
2024-01-12 20:03:11 +0530

    autocomplete removed

--- src/app/header/TopWalletExchangeBar.js
+++ src/app/header/TopWalletExchangeBar.js
@@ -1529,6 +1529,7 @@ class TopWalletExchangeBar extends Component {
                 )}
               </div>
               <input
+                autocomplete="off"
                 name={`wallet${1}`}
                 placeholder="Paste any wallet address or ENS here"
                 className="topBarContainerInputBlockInput"
