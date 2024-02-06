5a5d6cd48d673ee507da9be33f402f28c8686ece
adityakumarloch <aditya@loch.one>
2024-02-06 11:18:35 +0530

    transaction bug fixed

--- src/app/Portfolio/PortfolioMobile.js
+++ src/app/Portfolio/PortfolioMobile.js
@@ -572,7 +572,22 @@ class PortfolioMobile extends BaseReactComponent {
         tempCond.push(tempEle);
       }
     });
-
+    const arr = window.sessionStorage.getItem("addWallet")
+      ? JSON.parse(window.sessionStorage.getItem("addWallet"))
+      : [];
+    this.setState({
+      walletList: JSON.parse(window.sessionStorage.getItem("addWallet")),
+    });
+    let address = arr?.map((wallet) => {
+      return wallet.address;
+    });
+    tempCond = [
+      ...tempCond,
+      {
+        key: SEARCH_BY_WALLET_ADDRESS_IN,
+        value: address,
+      },
+    ];
     this.setState({ tableLoading: true });
     let data = new URLSearchParams();
     data.append("start", page * API_LIMIT);
@@ -1210,144 +1225,7 @@ class PortfolioMobile extends BaseReactComponent {
       isLoadingNft: false,
     });
   };
-  addConditionTransactionTable = (key, value) => {
-    if (key === "SEARCH_BY_TIMESTAMP_IN") {
-      const tempIsTimeUsed = this.state.isTimeSearchUsed;
-      TransactionHistoryYearFilter({
-        session_id: getCurrentUser().id,
-        email_address: getCurrentUser().email,
-        year_filter: value === "allYear" ? "All years" : value,
-        isSearchUsed: tempIsTimeUsed,
-      });
-      this.updateTimer();
-      this.setState({ isTimeSearchUsed: false, selectedTimes: value });
-    } else if (key === "SEARCH_BY_ASSETS_IN") {
-      let assets = [];
 
-      Promise.all([
-        new Promise((resolve) => {
-          if (value !== "allAssets") {
-            this.props.intelligenceState?.assetFilter?.map((e) => {
-              if (value?.includes(e.value)) {
-                assets.push(e.label);
-              }
-            });
-          }
-          resolve(); // Resolve the promise once the code execution is finished
-        }),
-      ]).then(() => {
-        const tempIsAssetUsed = this.state.isAssetSearchUsed;
-        TransactionHistoryAssetFilter({
-          session_id: getCurrentUser().id,
-          email_address: getCurrentUser().email,
-          asset_filter: value === "allAssets" ? "All assets" : assets,
-          isSearchUsed: tempIsAssetUsed,
-        });
-        this.updateTimer();
-        this.setState({ isAssetSearchUsed: false, selectedAssets: value });
-      });
-    } else if (key === "SEARCH_BY_METHOD_IN") {
-      this.setState({ selectedMethods: value });
-    } else if (key === "SEARCH_BY_CHAIN_IN") {
-      const tempIsNetworkUsed = this.state.isNetworkSearchUsed;
-      TransactionHistoryNetworkFilter({
-        session_id: getCurrentUser().id,
-        email_address: getCurrentUser().email,
-        network_filter: value === "allNetworks" ? "All networks" : value,
-        isSearchUsed: tempIsNetworkUsed,
-      });
-      this.updateTimer();
-      this.setState({ isNetworkSearchUsed: false, selectedNetworks: value });
-    }
-    let index = this.state.conditionTransHistory.findIndex(
-      (e) => e.key === key
-    );
-
-    let arr = [...this.state.conditionTransHistory];
-    let search_index = this.state.conditionTransHistory.findIndex(
-      (e) => e.key === SEARCH_BY_TEXT
-    );
-    if (
-      index !== -1 &&
-      value !== "allAssets" &&
-      value !== "allMethod" &&
-      value !== "allYear" &&
-      value !== "allNetworks" &&
-      value !== "allAmounts"
-    ) {
-      arr[index].value = value;
-    } else if (
-      value === "allAssets" ||
-      value === "allMethod" ||
-      value === "allYear" ||
-      value === "allNetworks" ||
-      value === "allAmounts"
-    ) {
-      if (index !== -1) {
-        arr.splice(index, 1);
-      }
-    } else {
-      let obj = {};
-      obj = {
-        key: key,
-        value: value,
-      };
-      arr.push(obj);
-    }
-    if (search_index !== -1) {
-      if (value === "" && key === SEARCH_BY_TEXT) {
-        arr.splice(search_index, 1);
-      }
-    }
-    // On Filter start from page 0
-    this.props.history.replace({
-      search: `?p=${START_INDEX}`,
-    });
-    this.setState({
-      conditionTransHistory: arr,
-    });
-  };
-  onKeyPressTransHistorySearch = (curKey) => {
-    if (curKey && curKey.code && curKey.code === "Enter") {
-      this.addConditionTransactionTable(SEARCH_BY_TEXT, this.state.search);
-      TransactionHistorySearch({
-        session_id: getCurrentUser().id,
-        email: getCurrentUser().email,
-        searched: this.state.search,
-      });
-    }
-  };
-  onChangeTransHistorySearchMethod = (ele) => {
-    this.setState({
-      search: ele.target.value,
-    });
-  };
-  handleNetworkSelectTransHistory = (badge) => {
-    if (badge && badge.length > 0) {
-      const tempArr = [];
-      if (badge[0]?.name !== "All") {
-        badge.forEach((resData) => tempArr.push(resData.id));
-      }
-      this.addConditionTransactionTable(
-        SEARCH_BY_CHAIN_IN,
-        tempArr && tempArr.length > 0 ? tempArr : "allNetworks"
-      );
-    }
-  };
-  handleAmount = (min, max) => {
-    if (!isNaN(min) && !isNaN(max)) {
-      this.setState(
-        {
-          minAmountTransHistory: min,
-          maxAmountTransHistory: max,
-        },
-        () => {
-          const value = { min_value: Number(min), max_value: Number(max) };
-          this.addConditionTransactionTable(SEARCH_BETWEEN_VALUE, value);
-        }
-      );
-    }
-  };
   render() {
     console.log(
       "this.props.intelligenceState.ProfitLossAsset",
