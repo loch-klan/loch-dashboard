1a9e12ee514c4d4a789c38107ba7a3c31b4520c0
adityakumarloch <aditya@loch.one>
2024-04-04 17:41:32 +0530

    bug fix

--- src/app/intelligence/InsightsPage.js
+++ src/app/intelligence/InsightsPage.js
@@ -152,7 +152,6 @@ class InsightsPage extends Component {
   };
   componentDidMount() {
     scrollToTop();
-    toast.error("Welcome to Insights ", { autoClose: 100000000 });
     if (
       !this.props.commonState.insight ||
       !this.props.intelligenceState.updatedInsightList
