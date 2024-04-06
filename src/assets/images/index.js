c633eb531408a1c076a56cc38f951972d3bbddad
adityakumarloch <aditya@loch.one>
2024-04-06 10:56:15 +0530

    dark mode changes

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 export { default as EditIcon } from "./EditIcon.svg";
 export { default as SmartMoneyAboutMobileImage } from "./SmartMoneyAboutMobile.png";
 export { default as SmartMoneyAboutImage } from "./SmartMoneyAbout.png";
+export { default as SmartMoneyAboutDarkImage } from "./SmartMoneyAboutDark.png";
 export { default as CopyClipboardIcon } from "./CopyClipboardIcon.svg";
